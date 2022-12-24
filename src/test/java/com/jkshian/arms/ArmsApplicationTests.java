c9fe4af239ace44e7eaaab15038b75577bca6025
ImeshaDilshani <imeshadilshani212@gmail.com>
2022-12-24 14:30:48 +0530

    Initial commit

---
+++ src/test/java/com/jkshian/arms/ArmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkshian.arms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
